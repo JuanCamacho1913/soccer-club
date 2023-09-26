98baaf566df2706a3177a6e334833aa5d5758732
¨juan <jecb1913@gmail.com>
2023-09-26 15:22:41 -0500

    Se agrego todas las entidades.

---
+++ src/main/java/com/example/club/models/Players.java
@@ -0,0 +1,23 @@
+package com.example.club.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "jugador")
+public class Players extends Base{
+
+    @Column(name = "apellido")
+    private String lastName;
+
+    @Column(name = "edad")
+    private Integer age;
+}
+
+
